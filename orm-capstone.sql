b4c6000810f4a130d4212b96fffca6c6b7e9f0b4
Luong Tran <126378143+luongtran97@users.noreply.github.com>
2023-12-07 10:59:21 +0900

    Add files via upload

---
+++ orm-capstone.sql
@@ -0,0 +1,103 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE `binh_luan` (
+  `binh_luan_id` int NOT NULL AUTO_INCREMENT,
+  `nguoi_dung_id` int DEFAULT NULL,
+  `hinh_id` int DEFAULT NULL,
+  `ngay_binh_luan` date DEFAULT NULL,
+  `noi_dung` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`binh_luan_id`),
+  KEY `hinh_id` (`hinh_id`),
+  KEY `nguoi_dung_id` (`nguoi_dung_id`),
+  CONSTRAINT `binh_luan_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`nguoi_dung_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `binh_luan_ibfk_2` FOREIGN KEY (`hinh_id`) REFERENCES `hinh_anh` (`hinh_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `hinh_anh` (
+  `hinh_id` int NOT NULL AUTO_INCREMENT,
+  `ten_hinh` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `duong_dan` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mo_ta` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nguoi_dung_id` int DEFAULT NULL,
+  PRIMARY KEY (`hinh_id`),
+  KEY `hinh_anh_ibfk_1` (`nguoi_dung_id`),
+  CONSTRAINT `hinh_anh_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`nguoi_dung_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `luu_anh` (
+  `luu_anh_id` int NOT NULL AUTO_INCREMENT,
+  `nguoi_dung_id` int DEFAULT NULL,
+  `hinh_id` int DEFAULT NULL,
+  `ngay_luu` date DEFAULT NULL,
+  `tinh_trang` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`luu_anh_id`),
+  KEY `hinh_id` (`hinh_id`),
+  KEY `nguoi_dung_id` (`nguoi_dung_id`),
+  CONSTRAINT `luu_anh_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoi_dung` (`nguoi_dung_id`),
+  CONSTRAINT `luu_anh_ibfk_2` FOREIGN KEY (`hinh_id`) REFERENCES `hinh_anh` (`hinh_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `nguoi_dung` (
+  `nguoi_dung_id` int NOT NULL AUTO_INCREMENT,
+  `taiKhoan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `matKhau` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `hoTen` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tuoi` int DEFAULT NULL,
+  `anh_dai_dien` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `face_app_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`nguoi_dung_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(1, 1, 1, '2023-11-12', '1');
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(2, 1, 1, '2023-11-12', 'd');
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(3, 1, 1, '2023-11-12', 'x');
+INSERT INTO `binh_luan` (`binh_luan_id`, `nguoi_dung_id`, `hinh_id`, `ngay_binh_luan`, `noi_dung`) VALUES
+(4, 1, 1, '2023-11-12', 'dá'),
+(5, 1, 1, '2023-11-13', '123'),
+(6, 1, 1, '2023-11-13', 'number1'),
+(10, 3, 1, '2023-11-13', 'hay qua troi');
+
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(1, 'abcdd', 'abncd.com.vn', 'hinh2', 1);
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(5, 'meo3', '1699890067381_meo.jpg', 'meo3', 1);
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(7, 'meo10', '1699892642793_meo.jpg', 'meo10', 3);
+INSERT INTO `hinh_anh` (`hinh_id`, `ten_hinh`, `duong_dan`, `mo_ta`, `nguoi_dung_id`) VALUES
+(8, 'abc ', '1700385438933_erd .png', 'abc', 1);
+
+INSERT INTO `luu_anh` (`luu_anh_id`, `nguoi_dung_id`, `hinh_id`, `ngay_luu`, `tinh_trang`) VALUES
+(1, 1, 1, '2023-11-12', 1);
+INSERT INTO `luu_anh` (`luu_anh_id`, `nguoi_dung_id`, `hinh_id`, `ngay_luu`, `tinh_trang`) VALUES
+(3, 2, 1, '2023-11-13', 1);
+INSERT INTO `luu_anh` (`luu_anh_id`, `nguoi_dung_id`, `hinh_id`, `ngay_luu`, `tinh_trang`) VALUES
+(4, 3, 1, '2023-11-13', 1);
+
+INSERT INTO `nguoi_dung` (`nguoi_dung_id`, `taiKhoan`, `email`, `matKhau`, `hoTen`, `tuoi`, `anh_dai_dien`, `face_app_id`) VALUES
+(1, 'test123', 'nguyen@gmail.com', '$2b$10$/.IPX5Bh4l3l27dkGzZeA.by6qAuWB9wmm0kQ5P60dwYB8vfJUMze', 'nguyen van abcd', 16, '1699850086374_fb.jpg', NULL);
+INSERT INTO `nguoi_dung` (`nguoi_dung_id`, `taiKhoan`, `email`, `matKhau`, `hoTen`, `tuoi`, `anh_dai_dien`, `face_app_id`) VALUES
+(2, '123', 'nguyenthid@gmail.com', '$2b$10$zSxisy9eZgLS3rDPsaoe9ubnZmAOoSY6ojI1fTnJZLufzWPvixziS', 'nguyen thi d', NULL, '1699890777772_meo.jpg', NULL);
+INSERT INTO `nguoi_dung` (`nguoi_dung_id`, `taiKhoan`, `email`, `matKhau`, `hoTen`, `tuoi`, `anh_dai_dien`, `face_app_id`) VALUES
+(3, '1234', 'nguyenvanz', '$2b$10$K1g/MvAyfSpTGrxvGz0QGulJSvWLrK49SCeCQWk9T50FMaWQKT18q', 'nguyenvanz', NULL, '1699892563861_meo.jpg', NULL);
+INSERT INTO `nguoi_dung` (`nguoi_dung_id`, `taiKhoan`, `email`, `matKhau`, `hoTen`, `tuoi`, `anh_dai_dien`, `face_app_id`) VALUES
+(4, '1234', 'nguyenvanq', '$2b$10$A1jWNuWUCmtrWtb7NINraOK.kOStYp1VLMvuHJwmLXDxrpTTbwYVi', 'nguyenvanq', NULL, NULL, NULL);
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
